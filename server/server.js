dc3e3920a90745ff5885f1d02266475fffe42172
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2025-07-28 10:07:21 +0530

    feat: add express server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express=require('express')
 const app=express()
 const dotenv=require('dotenv')
+const cors=require('cors')
+const helmet=require('helmet')
 const connectDB=require('./db/connectDB')
-
+//configure dotenv
+dotenv.config();
+const port=process.env.PORT || 3000
 
 
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
 
-dotenv.config();
+
+//connect database
 connectDB()
 
-app.listen(3000, ()=>{
-    console.log(`The server is running on port 3000`)
+app.get('/', (req,res)=>{
+    res.send("Hello, welcome to the todo API!")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running at: http://localhost:${port}`)
 })
